a0a9c23e53b6f85dfa24aecf9ba2ecc0803262e6
kim <jinxueli@imooc.com>
2021-05-17 19:53:58 +0800

    add networking

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -9,7 +9,30 @@ import Foundation
 
 extension RazeCore {
     
+    
+    /// Responsible for handling all networking calls
+    /// - Warning: Must create before using any public APIs
     public class Networking {
         
+        public class Manager {
+            
+            public init(){}
+            
+            private let session = URLSession.shared
+            
+            public func loadData(from url: URL, completionHandler:@escaping (NetworkResult<Data>) -> Void) {
+                let task = session.dataTask(with: url) {data,response,error in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completionHandler(result)
+                }
+                task.resume()
+            }
+        }
+        
+        public enum NetworkResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
     }
+    
 }
