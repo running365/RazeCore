fb3ac5d55f462569f589453c307ad32804227d9a
kim <jinxueli@imooc.com>
2021-05-18 10:46:53 +0800

    inject

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
+protocol NetworkSession {
+    func get(from url:URL, completionHandler:@escaping(Data?,Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: url) {data,_,error in
+            completionHandler(data,error)
+        }
+        task.resume()
+    }
+}
+
 extension RazeCore {
     
     
@@ -18,14 +31,17 @@ extension RazeCore {
             
             public init(){}
             
-            private let session = URLSession.shared
+            internal var session:NetworkSession = URLSession.shared
             
+            /// Calls to the live inernet to retrieve data from data form
+            /// - Parameters:
+            ///   - url: the location you wish to fetch data from
+            ///   - completionHandler: returns a result object which signifies the status of the request
             public func loadData(from url: URL, completionHandler:@escaping (NetworkResult<Data>) -> Void) {
-                let task = session.dataTask(with: url) {data,response,error in
+                session.get(from: url) { (data, error) in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completionHandler(result)
                 }
-                task.resume()
             }
         }
         
@@ -36,3 +52,14 @@ extension RazeCore {
     }
     
 }
+
+
+/*
+     private let session = URLSession.shared
+
+     let task = session.dataTask(with: url) {data,response,error in
+         let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+         completionHandler(result)
+     }
+     task.resume()
+ */
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -8,9 +8,41 @@
 import XCTest
 @testable import RazeCore
 
+class NetworkSessionMock: NetworkSession {
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data,error)
+    }
+
+}
+
 final class RazeNetworkingTests: XCTestCase {
     
-    
+    func testLoadDataCall() {
+        let manager = RazeCore.Networking.Manager()
+        let session = NetworkSessionMock()
+        manager.session = session
+        let expectation = XCTestExpectation(description: "Called for data")
+        
+        let data = Data([0,1,0,1])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
+        
+        manager.loadData(from:url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                XCTAssertEqual(data, returnedData, "mananger returned unexpected data")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        wait(for:[expectation],timeout:10)
+    }
+
+  /*
     func testLoadDataCall() {
         let manager = RazeCore.Networking.Manager()
         let expectation = XCTestExpectation(description: "Called for data")
@@ -29,7 +61,7 @@ final class RazeNetworkingTests: XCTestCase {
         }
         wait(for:[expectation],timeout:10)
     }
-    
+    */
     
     static var allTests = [
         ("testLoadDataCall", testLoadDataCall),
